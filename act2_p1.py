3ea6d6e743d9ef45e2327ab8a59cbdf7a442fccd
Navuzasshu <yachimaeee@gmail.com>
2021-09-17 09:27:57 +0000

    Test

--- act2_p1.py
+++ act2_p1.py
@@ -1,9 +1,12 @@
 print("This program will print student averages. Enter your scores! ")
+
+#Initialize Variables 
 prelims=float(input("Enter your prelims score: "))
 midterms=float(input("Enter your midterms score: "))
 semis=float(input("Enter your semifinals score: "))
 finals=float(input("Enter your finals score: "))
 
+#compute average
 avg=(prelims+midterms+semis+finals)/4
 
 print("Your average is {}!".format(avg))
