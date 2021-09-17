dada5a9276098c8f5bae957c91598868766fce89
NetAcad DEVASC <changeme@example.com>
2021-09-17 09:20:00 +0000

    First Part

---
+++ act2_p1.py
@@ -0,0 +1,9 @@
+print("This program will print student averages. Enter your scores! ")
+prelims=float(input("Enter your prelims score: "))
+midterms=float(input("Enter your midterms score: "))
+semis=float(input("Enter your semifinals score: "))
+finals=float(input("Enter your finals score: "))
+
+avg=(prelims+midterms+semis+finals)/4
+
+print("Your average is {}!".format(avg))
